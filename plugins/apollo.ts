32d18f9f0e6ec24637f477eb7dcb0ab8f295f235
user04 <slavko.savic@me.com>
2024-06-12 20:03:31 +0200

    GNJIDO  NE ZNAŠ KO JE VLASNIK PROJEKTA
    JAVI SE GNJIDO

--- plugins/apollo.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  // access cookie for auth
-  nuxtApp.hook("apollo:auth", ({ client, token }) => {
-    const auth = useStoreApiAuth();
-    // apply apollo client token
-    token.value = auth.token$;
-  });
-});
--- plugins/app-mounted.ts
+++
@@ -1,7 +0,0 @@
-// #https://nuxt.com/docs/getting-started/error-handling#vue-rendering-lifecycle
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook("app:mounted", () => {
-    const appMounted$ = useAppMounted();
-    appMounted$.value = true;
-  });
-});
