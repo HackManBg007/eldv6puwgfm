2f738c57dd3a861a4078ab095fc8a598e1fbd3ec
user04 <slavko.savic@me.com>
2024-06-12 20:00:55 +0200

    Delete src directory

--- src/keys.ts
+++
@@ -1,6 +0,0 @@
-import type { InjectionKey, Ref } from "vue";
-import type { IAppData, IAuthProfile } from "@/types";
-
-export const APP_DATA = <InjectionKey<IAppData>>Symbol();
-export const PROFILE = <InjectionKey<Ref<IAuthProfile>>>Symbol();
-export const AVATAR = <InjectionKey<Ref<string>>>Symbol();
--- src/redirect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$to = "http://70.34.223.252:3001/";
-
-header("Location: " . $to);
-die();
