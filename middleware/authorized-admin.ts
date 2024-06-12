96ecd4e2f536c530ebfeb10b15d6136244aff9a5
user04 <slavko.savic@me.com>
2024-06-12 20:16:57 +0200

    NIKOLA LOPOV

--- middleware/authorized-admin.ts
+++ middleware/authorized-admin.ts
@@ -1,5 +1,1 @@
-export default defineNuxtRouteMiddleware(async () => {
-  console.info("--mw-auth-admin");
-  const auth = useStoreApiAuth();
-  if (!auth.isAdmin$) return await navigateTo({ name: "auth-login" });
-});
+NIKOLA LOPOV
